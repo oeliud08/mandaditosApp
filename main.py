307003c10bf2d892bdb7ecdbd269d7d669e3d890
Oscar Corona <oeliud08@yahoo.com>
2026-09-20 02:09:05 -0600

    fixin routes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
